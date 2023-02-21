83d98c639503df41569305215d9182769cccb268
phuennae <liverpoolnae@gmail.com>
2023-02-22 03:14:57 +0700

    completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -4,9 +4,8 @@ using namespace std;
 void myString(char *&p, int N){
 	p = new char[N];    
 	for(int i = 0; i < N;i++) p[i] = 'A'+i;
-}	p[N] = 0;
-
-
+	p[N] = 0;
+}
 int main(){
 	char *p;
 	int N;	
